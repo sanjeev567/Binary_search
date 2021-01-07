0e07f322f7bef83a2a1e9d420fd659b35b9c2eae
sanjeeev <sanjeevsingh.ss342@gmail.com>
2021-01-07 20:29:20 +0530

    Binary search

---
+++ tut2.cpp
@@ -0,0 +1,30 @@
+//<---------------Binary Search------------------>
+//array must be sorted
+#include<iostream>
+using namespace std;
+
+int Binary_sear(int* A, int n, int x){
+    int start=0,end=(n-1);
+    while(start<=end){
+        int mid =(start+end)/2;
+    if (A[mid]==x)
+        return mid;
+    else if(x<A[mid])
+        end= mid -1;
+    else 
+        start=mid +1;
+    }
+    return -1;
+}
+int main(){
+    int A[]={1,2,3,4,5,6,7,8};
+    int n=sizeof(A)/sizeof(A[2]);
+    int x=8;
+    int res=Binary_sear(A,n,x);
+    if (res==-1)
+        cout<<"not found in the array";
+    else
+        cout<<"got it";
+        return 0;
+    
+}
